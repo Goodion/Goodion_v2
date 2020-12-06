f46e5f27b32aae99e725083e75a84a1016d8c124
Ivan Saltykov <hcys@list.ru>
2020-12-06 18:17:30 +0300

    header, footer refactoring. svg icons add

--- config/config.php
+++ config/config.php
@@ -3,4 +3,14 @@
 return [
     'admin_email' => env('ADMIN_EMAIL'),
     'admin_password' => env('ADMIN_PASSWORD'),
+    'goodion_email' => env('GOODION_EMAIL'),
+    'goodion_phone' => env('GOODION_PHONE'),
+    'goodion_email_link' => env('GOODION_EMAIL_LINK'),
+    'goodion_phone_link' => env('GOODION_PHONE_LINK'),
+    'goodion_whatsapp' => env('GOODION_WHATSAPP'),
+    'goodion_viber' => env('GOODION_VIBER'),
+    'goodion_telegram' => env('GOODION_TELEGRAM'),
+    'goodion_instagram' => env('GOODION_INSTAGRAM'),
+    'goodion_facebook' => env('GOODION_FACEBOOK'),
+    'goodion_vk' => env('GOODION_VK'),
 ];
